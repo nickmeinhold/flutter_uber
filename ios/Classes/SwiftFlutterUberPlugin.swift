e1ab78f43d4a1479ccc8d6157e2b13041d034e5a
Nick Meinhold <nick.meinhold@gmail.com>
2019-12-27 23:24:46 +1100

    Add login call
    - added call to method channel
    - used LoginManager to perform login

--- ios/Classes/SwiftFlutterUberPlugin.swift
+++ ios/Classes/SwiftFlutterUberPlugin.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import UberCore
 
 public class SwiftFlutterUberPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -12,6 +13,28 @@ public class SwiftFlutterUberPlugin: NSObject, FlutterPlugin {
     
     if (call.method == "getPlatformVersion") {
         result("iOS " + UIDevice.current.systemVersion)
+        return
+    }
+
+    if (call.method == "login") {
+        
+      let storyboard : UIStoryboard? = UIStoryboard.init(name: "Main", bundle: nil);
+      let flutterVC: UIViewController? = storyboard!.instantiateViewController(withIdentifier: "FlutterViewController")
+        
+      let loginManager = LoginManager()
+      loginManager.login(requestedScopes:[.request], presentingViewController: flutterVC, completion: { accessToken, error in
+        // Completion block. If accessToken is non-nil, you’re good to go
+        // Otherwise, error.code corresponds to the RidesAuthenticationErrorType that occured
+        if(accessToken == nil) {
+            result("\(error!.description)")
+        }
+        result(accessToken)
+      })
+      return
+    }
+
+    if (call.method == "getEstimate") {
+        result("5000")
         return
     }
     
--- lib/flutter_uber.dart
+++ lib/flutter_uber.dart
@@ -3,11 +3,20 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class FlutterUber {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_uber');
+  static const MethodChannel _channel = const MethodChannel('flutter_uber');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  static Future<String> get login async {
+    final String estimate = await _channel.invokeMethod('login');
+    return estimate;
+  }
+
+  static Future<String> get estimate async {
+    final String estimate = await _channel.invokeMethod('getEstimate');
+    return estimate;
   }
 }
